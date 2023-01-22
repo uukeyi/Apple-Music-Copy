19959cb4e4904e1c1504b65d0c9e8dd93d3747b5
Aleksandr <119818692+uukeyi@users.noreply.github.com>
2023-01-23 01:48:25 +0300

    Base API

---
+++ src/API.js
@@ -0,0 +1,4 @@
+export const MUSIC_API = 'http://localhost:8000/music'
+export const USERS_API = 'http://localhost:8000/users'
+export const SUBSCRIPTIONS_API = 'http://localhost:8000/subscriptions'
+export const ARTISTS_API = 'http://localhost:8000/artists'
